a1b6fe877a79d9f262ad3a4bf4034dc3fcef4366
Xamz <bugi@gmail.com>
2024-06-06 16:58:16 +0300

    init commit

---
+++ src/main/java/web/controller/CarsConrtoller.java
@@ -0,0 +1,24 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+
+@Controller
+public class CarsConrtoller {
+    private final CarService carService;
+
+    @Autowired
+    public CarsConrtoller(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping(value = "/cars")
+    public String printLimitedCars(@RequestParam(value = "count", defaultValue = "5") Integer count, Model model) {
+        model.addAttribute("cars", carService.carList(count));
+        return "cars";
+    }
+}
